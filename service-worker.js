1d6d22c751da672d9163982bc09bc7852f7970c3
Sang Nguyen <nnsang24@gmail.com>
2020-07-17 21:46:07 +0700

    cache html

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
-    prefix: 'yem',
-    suffix: 'v1',
-    precache: 'precache',
-    runtime: 'runtime-cache'
+	prefix: 'yem',
+	suffix: 'v2',
+	precache: 'precache',
+	runtime: 'runtime-cache',
 });
 
 // let Service Worker take control of pages ASAP
@@ -14,19 +14,21 @@ workbox.clientsClaim();
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
 // use `networkFirst` strategy for `*.html`, like all my posts
-workbox.routing.registerRoute(
-    /\.html$/,
-    workbox.strategies.networkFirst()
-);
+workbox.routing.registerRoute(/\.html$/, workbox.strategies.networkFirst());
 
 // use `cacheFirst` strategy for images
 workbox.routing.registerRoute(
-    /assets\/(img|icons)/,
-    workbox.strategies.cacheFirst()
+	/assets\/(img|icons)/,
+	workbox.strategies.cacheFirst()
 );
 
 // third party files
 workbox.routing.registerRoute(
-    /^https?:\/\/cdn.staticfile.org/,
-    workbox.strategies.staleWhileRevalidate()
+	/^https?:\/\/cdn.staticfile.org/,
+	workbox.strategies.staleWhileRevalidate()
+);
+
+workbox.routing.registerRoute(
+	/.*\.(?:png|jpg|jpeg|svg|gif|html)/,
+	workbox.strategies.cacheFirst()
 );
